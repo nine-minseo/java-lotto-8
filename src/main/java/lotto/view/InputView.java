9c192c5c7a334652a7fcf395a32273aabea2fa7f
구민서 <minseo12379@gmail.com>
2025-10-29 21:10:47 +0900

    feat: 로또 구입 금액 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,13 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String PROMPT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+
+    public static String getPurchaseAmount() {
+        System.out.println(PROMPT_PURCHASE_AMOUNT);
+
+        return Console.readLine();
+    }
 }
