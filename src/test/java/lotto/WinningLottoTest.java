792f6741311147d55cb737a44fce7cff1adc3702
구민서 <minseo12379@gmail.com>
2025-11-03 19:09:33 +0900

    test: WinningLotto 등수 판별  테스트 구현

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
 import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class WinningLottoTest {
     @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
@@ -27,5 +30,62 @@ class WinningLottoTest {
         org.junit.jupiter.api.Assertions.assertDoesNotThrow(
                 () -> new WinningLotto(winningNumbers, bonusNumber)
         );
+    }
+
+    private WinningLotto winningLotto;
+
+    @BeforeEach
+    void setUp() {
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+    }
+
+    @DisplayName("6개가 일치하는지 판별한다.")
+    @Test
+    void matchRank1() {
+        Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Optional<LottoRank> result = winningLotto.match(userLotto);
+        assertThat(result).isEqualTo(Optional.of(LottoRank.SIX_MATCH));
+    }
+
+    @DisplayName("5개와 보너스 번호가 일치하는지 판별한다.")
+    @Test
+    void matchRank2() {
+        Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Optional<LottoRank> result = winningLotto.match(userLotto);
+        assertThat(result).isEqualTo(Optional.of(LottoRank.FIVE_BONUS_MATCH));
+    }
+
+    @DisplayName("5개가 일치하는지 판별한다.")
+    @Test
+    void matchRank3() {
+        Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        Optional<LottoRank> result = winningLotto.match(userLotto);
+        assertThat(result).isEqualTo(Optional.of(LottoRank.FIVE_MATCH));
+    }
+
+    @DisplayName("4개가 일치하는지 판별한다.")
+    @Test
+    void matchRank4() {
+        Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 8, 9));
+        Optional<LottoRank> result = winningLotto.match(userLotto);
+        assertThat(result).isEqualTo(Optional.of(LottoRank.FOUR_MATCH));
+    }
+
+    @DisplayName("3개가 일치하는지 판별한다.")
+    @Test
+    void matchRank5() {
+        Lotto userLotto = new Lotto(List.of(1, 2, 3, 8, 9, 10));
+        Optional<LottoRank> result = winningLotto.match(userLotto);
+        assertThat(result).isEqualTo(Optional.of(LottoRank.THREE_MATCH));
+    }
+
+    @DisplayName("2개 이하로 일치하면 Optional.empty()를 반환한다.")
+    @Test
+    void matchMiss() {
+        Lotto userLotto = new Lotto(List.of(1, 2, 8, 9, 10, 11));
+        Optional<LottoRank> result = winningLotto.match(userLotto);
+        assertThat(result).isEmpty();
     }
 }
