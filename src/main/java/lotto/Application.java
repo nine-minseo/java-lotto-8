bf0934276bb430ae68a191f437421943bfd1796b
구민서 <minseo12379@gmail.com>
2026-01-07 12:00:10 +0900

    Feat: 로또 구입 금액 입력받는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-
+        String input = InputView.readPurchaseAmount();
     }
 }
