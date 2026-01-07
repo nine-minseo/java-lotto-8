e5a73d3d769e9a8e1df4f15983d5f42a9f005c68
구민서 <minseo12379@gmail.com>
2026-01-07 22:15:42 +0900

    feat: 로또 번호 중복 확인 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,9 @@
 package lotto;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import javax.print.attribute.standard.NumberUp;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -24,6 +25,14 @@ public class Lotto {
     private void validateLottoNumberCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
